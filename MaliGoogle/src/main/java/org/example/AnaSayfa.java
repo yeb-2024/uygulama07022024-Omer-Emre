fbbd6930d7ec4f99fe5c7e4c470615751ef6e1f6
ASUS <omeremreu361@gmail.com>
2025-02-07 18:29:40 +0300

    anasayfa oluşturuldu

---
+++ MaliGoogle/src/main/java/org/example/AnaSayfa.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javax.swing.*;
+
+public class AnaSayfa {
+
+    JFrame frame = new JFrame();
+
+    public AnaSayfa()
+    {
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("AnaSayfa");
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
