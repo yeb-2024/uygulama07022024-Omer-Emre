3675631eea7d44c41a02dc03b3d16c0787f05423
ASUS <omeremreu361@gmail.com>
2025-02-07 19:02:20 +0300

    profil sayfasındaki bilgiler yapılarark uygulama bitirildi

--- MaliGoogle/src/main/java/org/example/AnaSayfa.java
+++ MaliGoogle/src/main/java/org/example/AnaSayfa.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
--- MaliGoogle/src/main/java/org/example/ProfilSayfası.java
+++ MaliGoogle/src/main/java/org/example/ProfilSayfası.java
@@ -6,12 +6,26 @@ import java.awt.*;
 public class ProfilSayfası {
     JFrame frame = new JFrame();
     JLabel label = new JLabel();
+    JLabel imagelabel = new JLabel();
+    JLabel aciklamalabel = new JLabel();
 
     public ProfilSayfası()
     {
         label.setText("Isım : Fatih Sultan Mehmet");
         label.setFont(new Font(null,Font.PLAIN,25));
 
+        ImageIcon imageIcon = new ImageIcon("images.jpeg");
+        Image newımg = imageIcon.getImage().getScaledInstance(150,150,Image.SCALE_SMOOTH);
+        ImageIcon newımage = new ImageIcon(newımg);
+
+        aciklamalabel.setText("fatih sultan mehmet istanbulu \n" +
+                              "fethederek çağ açıp çağ kapatmıştır ");
+        aciklamalabel.setFont(new Font(null,Font.BOLD,25));
+
+        frame.add(aciklamalabel);
+
+        imagelabel.setIcon(newımage);
+        frame.add(imagelabel);
         frame.add(label);
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
