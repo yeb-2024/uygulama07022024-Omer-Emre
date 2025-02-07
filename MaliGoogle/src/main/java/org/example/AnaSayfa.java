96c792e099d1591a22e4151c11a3b9a0a1ab38fd
ASUS <omeremreu361@gmail.com>
2025-02-07 18:34:56 +0300

    buton oluşsturuldu

--- MaliGoogle/src/main/java/org/example/AnaSayfa.java
+++ MaliGoogle/src/main/java/org/example/AnaSayfa.java
@@ -1,17 +1,30 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AnaSayfa {
+public class AnaSayfa  implements ActionListener {
 
     JFrame frame = new JFrame();
+    JButton buton = new JButton("Profil Göster");
+
 
     public AnaSayfa()
     {
+        buton.addActionListener(this);
+        buton.setBounds(200,200,100,50);
+
+        frame.add(buton);
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("AnaSayfa");
         frame.setLayout(null);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
